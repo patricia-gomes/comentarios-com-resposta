9fcbf245ad3683df4e756afe4eb735dce91a985f
Patricia Gomes <patriciagomesro33@gmail.com>
2020-06-17 21:42:19 -0400

    A pasta onde esta o html do cabeçalho e o rodape do site

---
+++ views/footer.php
@@ -0,0 +1,7 @@
+	<footer></footer>
+	<!-- JQuery  -->
+	<script type="text/javascript" src="js/jquery.min.js"></script>
+	<!--- JS --->
+	<script src="js/script.js"></script>
+</body>
+</html>
---
+++ views/header.php
@@ -0,0 +1,8 @@
+<!DOCTYPE html>
+<html>
+<head>
+	<meta charset="utf-8">
+	<title>Sistema de Comentários e Resposta | Patricia Gomes</title>
+	<link rel="stylesheet" href="css/style.css" >
+</head>
+<body>
